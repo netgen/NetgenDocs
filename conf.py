d4e3bd54609d5875463d48e1fbf705c915dd5731
Edi Modrić <edi@netgen.hr>
2017-06-27 17:09:16 +0200

    Remove the search box

--- conf.py
+++ conf.py
@@ -25,8 +25,8 @@ exclude_patterns = ['_build']
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-# html_static_path = ['_static']
-# templates_path = ['_templates']
+html_static_path = ['_static']
+templates_path = ['_templates']
 
 html_theme_options = {
     'collapse_navigation': True,
